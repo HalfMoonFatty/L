6e90b80a37840867ec3ce23124bfa6cc3cb424a1
HalfMoonFatty <allofotherstuff@gmail.com>
2017-09-17 18:44:34 -0700

    Update H2O.py

--- H2O.py
+++ H2O.py
@@ -1,4 +1,38 @@
+        
+"""h2o lock everything"""
 
+from threading import Condition
+
+class H2O(object):
+  def __init__(self):
+    self.h = 0
+    self.o = 0
+    self.cv = Condtion()    
+
+  def H(self):
+    with self.cv:
+      self.h += 1
+      while self.h < 2 and self.o < 1:
+        self.cv.wait()
+      self.h -= 2
+      self.o -= 1
+      if self.h >= 2 and self.o >= 1:
+        self.cv.notifyAll()
+
+  def O(self):
+    with self.cv:
+      self.o += 1
+      while self.h < 2 and self.o < 1:
+        self.cv.wait()
+      self.h -= 2
+      self.o -= 1
+      if self.h >= 2 and self.o >= 1:
+        self.cv.notifyAll()
+
+        
+        
+        
+        
 
 
 from threading import Condition
@@ -43,35 +77,5 @@ class H2O(object):
         self.O_cv.release()
         self.O_cv.notifyAll()
 
-        
-"""h2o lock everything"""
-
-from threading import Condition
-
-class H2O(object):
-  def __init__(self):
-    self.h = 0
-    self.o = 0
-    self.cv = Condtion()    
-
-  def H(self):
-    with self.cv:
-      self.h += 1
-      while self.h < 2 and self.o < 1:
-        self.cv.wait()
-      self.h -= 2
-      self.o -= 1
-      if self.h >= 2 and self.o >= 1:
-        self.cv.notifyAll()
-
-  def O(self):
-    with self.cv:
-      self.o += 1
-      while self.h < 2 and self.o < 1:
-        self.cv.wait()
-      self.h -= 2
-      self.o -= 1
-      if self.h >= 2 and self.o >= 1:
-        self.cv.notifyAll()
 
         
